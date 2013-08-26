81a356e9642a3845a1c49706fb40e942a2043e53
Mark Embling <contact@markembling.info>
2013-08-26 21:47:24 +0100

    Tidy up utils

--- pyobdlib/utils.py
+++ pyobdlib/utils.py
@@ -1,48 +1,78 @@
+# pyobdlib
+# Copyright (C) 2004 Donour Sizemore
+# Copyright (C) 2009 Secons Ltd.
+# Copyright (C) 2013 Mark Embling
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import serial
 import platform
 
 def scan_serial():
-    """scan for available ports. return a list of serial names"""
-    available = []
-    for i in range(256):
-      try: #scan standart ttyS*
-        s = serial.Serial(i)
-        available.append(s.portstr)
-        s.close()   # explicit close 'cause of delayed GC in java
-      except serial.SerialException:
-        pass
-    for i in range(256):
-      try: #scan USB ttyACM
-        s = serial.Serial("/dev/ttyACM"+str(i))
-        available.append(s.portstr)
-        s.close()   # explicit close 'cause of delayed GC in java
-      except serial.SerialException:
-        pass
-    for i in range(256):
-      try:
-        s = serial.Serial("/dev/ttyUSB"+str(i))
-        available.append(s.portstr)
-        s.close()   # explicit close 'cause of delayed GC in java
-      except serial.SerialException:
-        pass
-    for i in range(256):
-      try:
-        s = serial.Serial("/dev/ttyd"+str(i))
-        available.append(s.portstr)
-        s.close()   # explicit close 'cause of delayed GC in java
-      except serial.SerialException:
-        pass
-        
-    # ELM-USB shows up as /dev/tty.usbmodemXXXX, where XXXX is a changing hex string
-    # on connection; so we have to search through all 64K options
-    if len(platform.mac_ver()[0])!=0:  #search only on MAC
-      for i in range (65535):
-        extension = hex(i).replace("0x","", 1)
-        try:
-          s = serial.Serial("/dev/tty.usbmodem"+extension)
-          available.append(s.portstr)
-          s.close()
-        except serial.SerialException:
-          pass 
+    """Scan for available serial ports and return a list of available names"""
     
-    return available
+    available_ports = []
+    
+    for i in range(256):
+        # Scan standard ttyS* or COM* on Windows
+        try:
+            s = serial.Serial(i)
+            available_ports.append(s.portstr)
+            s.close()
+        except serial.SerialException:
+            pass
+        
+    for i in range(256):
+        # Scan USB ttyACM*
+        try:
+            s = serial.Serial("/dev/ttyACM"+str(i))
+            available_ports.append(s.portstr)
+            s.close()
+        except serial.SerialException:
+            pass
+
+    for i in range(256):
+        # Scan ttyUSB*
+        try:
+            s = serial.Serial("/dev/ttyUSB"+str(i))
+            available_ports.append(s.portstr)
+            s.close()
+        except serial.SerialException:
+            pass
+
+    for i in range(256):
+        # Scan ttyd*
+        try:
+            s = serial.Serial("/dev/ttyd"+str(i))
+            available_ports.append(s.portstr)
+            s.close()
+        except serial.SerialException:
+            pass
+        
+    # Under OS X, ELM-USB shows up as /dev/tty.usbmodemXXXX, where XXXX 
+    # is a changing hex string on connection; so we have to search through 
+    # all 64K options
+    if len(platform.mac_ver()[0])!=0:  #search only on Mac
+        for i in range (65535):
+            extension = hex(i).replace("0x","", 1)
+            try:
+                s = serial.Serial("/dev/tty.usbmodem"+extension)
+                available_ports.append(s.portstr)
+                s.close()
+            except serial.SerialException:
+                pass 
+    
+    return available_ports
+
